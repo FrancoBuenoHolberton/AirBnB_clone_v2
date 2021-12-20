7030a8167ed488936702302f1a1296eae6dd3000
root <3290@holbertonschool.com>
2021-12-20 09:12:45 -0800

    3-4 and Authors

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+-- Create database , New User
+-- And Grant Privileges in tables and db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
