8ba39bce76cabd45bf85d319e2845c6b894ca174
root <3290@holbertonschool.com>
2022-01-13 06:48:16 -0800

    1Prueba

---
+++ 1-pack_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+1. Compress before sending
+"""
+from fabric.api import *
+from datetime import *
+
+def do_pack():
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(name))
+        return name
+    except:
+        return none
