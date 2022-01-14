4acae4b7e9513c92244f00ad1a41a8f5c8348b9c
root <3290@holbertonschool.com>
2022-01-13 16:30:12 -0800

    fix2line

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,6 +6,7 @@ from fabric.api import *
 from os.path import *
 env.hosts = ['34.138.247.205', '34.74.60.97']
 
+
 def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
@@ -16,7 +17,7 @@ def do_deploy(archive_path):
             path = "/data/web_static/releases/"
             put(archive_path, '/tmp/')
             run('mkdir -p {}{}/'.format(filename, extension))
-            run('tar -xzf /tmp/{} -C {}{}/',format(filename, path, extension))
+            run('tar -xzf /tmp/{} -C {}{}/'.format(filename, path, extension))
             run('rm /tmp/{}'.format(filename))
             run('mv {0}{1}/web_static/* {0}{1}/'.format(filename, extension))
             run('rm -rf {}{}/web_static'.format(path, extension))
